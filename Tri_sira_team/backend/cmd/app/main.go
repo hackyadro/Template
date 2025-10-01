8b778421456cc2547c1a6707529f20a6e1532af5
UnemployedTocha <UnemployedTocha@mail.ru>
2025-10-02 01:12:56 +0700

    Add: logger setup && client init

--- Tri_sira_team/backend/cmd/app/main.go
+++ Tri_sira_team/backend/cmd/app/main.go
@@ -1,7 +1,30 @@
-package mai
+package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+	m "service/internal/mosquitto"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// Настройка логгера
+	log := setupLogger()
+
+	// Создание клиента для MQTT брокера, по сути это как консьюмер для кафки
+	var cfg m.Config
+	cfg.Broker = "mosquitto:" + os.Getenv("MOSQUITTO_INTERNAL_PORT")
+	cfg.ClientId = "service"
+	cfg.Username = os.Getenv("MOSQUITTO_USER")
+	cfg.Password = os.Getenv("MOSQUITTO_PASSWORD")
+	_, err := m.NewClient(cfg, log)
+	if err != nil {
+		log.Error("creating broker client error", slog.Any("error: %w", err))
+	}
+	log.Info("service connected to broker")
+
+}
+
+func setupLogger() (log *slog.Logger) {
+	log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	return
 }
